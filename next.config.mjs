d4d1117e1d7812b3858b61a03b8531330f27436e
Piyush Bagul <piyushvbagul916@gmail.com>
2024-10-16 14:02:29 +0530

    initialized

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com",
+      "images.unsplash.com",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,45 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        aspekta: ["var(--font-aspekta)", "sans-serif"],
+      },
       colors: {
+        // Base colors
         background: "var(--background)",
         foreground: "var(--foreground)",
+        foreground2: "var(--foreground2)",
+        foreground3: "var(--foreground3)",
+        muted: "var(--muted)",
+        border: "var(--border)",
+
+        primary: "var(--primary)",
+        primaryHover: "var(--primary-hover)",
+        secondary: "var(--secondary)",
+        tertiary: "var(--tertiary)",
+
+        blu: "var(--blu)",
+        org: "var(--org)",
+        grn: "var(--grn)",
+        peach: "var(--peach)",
+
+        blackBg: "var(--blackBg)",
+        blackFg: "var(--blackFg)",
+        blackPri: "var(--blackPri)",
+
+        graySecondary: "var(--gray-secondary)",
+        graySecondaryForeground: "var(--gray-secondary-foreground)",
+        graySecondaryForegroundMuted: "var(--gray-secondary-foreground-muted)",
+
+        purple: "#A155E0",
+        lightPurple: "#E3B4FF",
+        hoverPaste: "#5A5A5A",
+        mutedGray: "#797878",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
